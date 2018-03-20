99f7f6468207cefb79d32b41f95255a2d6ef76ce
CodeInPixel Studios <codeinpixel@gmail.com>
2018-03-20 04:19:19 -0300

    Animação do Logo refeita e finalizada!

--- src/app/core/animation/logo.animation.ts
+++ src/app/core/animation/logo.animation.ts
@@ -1,132 +1,104 @@
 import * as $ from 'jquery';
 import { TweenLite, TimelineLite, Back } from 'gsap';
 
-let getShowText,
-    getDuration,
-    getLogoBase,
-    getCenterPixel,
-    getTagLeft,
-    getTagRight,
-    getTags,
-    getText,
-    getSubText,
-    getPixelback,
-    getPixelbackColor,
-    animationProgress = false,
-    animationInit     = false;
-
-// GSPA Init
-const tl0      = new TimelineLite(),
-      tl1      = new TimelineLite(),
-      tl2      = new TimelineLite(),
-      tl3      = new TimelineLite(),
-      tl4      = new TimelineLite(),
-      tl5      = new TimelineLite(),
-      tl6      = new TimelineLite(),
-      tl7      = new TimelineLite(),
-      easeEff  = Back.easeOut.config(2),
-      easeEff2 = Back.easeInOut.config(2);
-
-// Reset logo
-const resetLogo = () => {
-  tl0.set(getLogoBase, {opcity: 0});
-  tl1.set(getCenterPixel, {scale: 0, rotation: 0});
-  tl2.set( getTags, {scale: 0, opacity: 0, x: '-10%'});
-  tl3.set( getPixelback, {scale: 0, rotation: 0, opacity: 0});
-  tl4.set( getText, {opacity: 0, top: '100%'});
-  tl5.set( getSubText, {opacity: 0, top: '1000%'});
-};
-
-// Logo Init
-const LogoAnimation = (logoElement, duration = 1, showText) => {
-  getShowText       = showText;
-  getLogoBase       = $(`#${logoElement}`);
-  getCenterPixel    = $(`#${logoElement} ._logo_pixel_center`);
-  getTagLeft        = $(`#${logoElement} ._logo_tag_left`);
-  getTagRight       = $(`#${logoElement} ._logo_tag_right`);
-  getTags           = $(`#${logoElement} ._logo_tag_left, #${logoElement} ._logo_tag_right`);
-  getText           = $(`#${logoElement} ._logo_text`);
-  getSubText        = $(`#${logoElement} ._logo_subtext`);
-  getPixelback      = $(`#${logoElement} ._logo_pixel_back`);
-  getPixelbackColor = $(`#${logoElement} ._logo_pixel_back ._logo_pixel_back_color`);
-  getDuration       = duration;
-
- // Init
- resetLogo();
- tl0.pause();
- tl1.pause();
- tl2.pause();
- tl3.pause();
- tl4.pause();
- tl5.pause();
+const resetLogoStyle = () => {
+    TweenLite.set( this.getCenterPixel, {scale: 0, rotation: 0} );
+    TweenLite.set( this.getTags, {scale: 0, opacity: 0, x: '-10%'} );
+    TweenLite.set( this.getPixelback, {scale: 0, rotation: 0, opacity: 0} );
+    if (this[`showText_${this.elementId}`] === 'true') {
+      TweenLite.set( this.getText, {opacity: 0, top: '100%'} );
+      TweenLite.set( this.getSubText, {opacity: 0, top: '1000%'} );
+    }
 };
 
 const pixelShine = () => {
   // PixelBack Shine
-  tl6.staggerTo( getPixelbackColor, 2, {
-    ease: easeEff2,
+  this[`tl2${this.elementId}`].staggerTo( this.getPixelbackColor, 2, {
+    ease: this.easeEff2,
     backgroundColor: 'rgba(255, 255, 255, 0.3)',
     borderColor: 'rgba(255, 255, 255, 0.3)',
-    repeat: 10,
+    repeat: 9,
     repeatDelay: 5
   }, .05);
-  tl7.staggerTo( getPixelbackColor, 2, {
-    ease: easeEff2,
+  this[`tl3${this.elementId}`].staggerTo( this.getPixelbackColor, 2, {
+    ease: this.easeEff2,
     backgroundColor: 'rgba(46, 70, 92, 0.28)',
     borderColor: 'rgba(46, 70, 92, 0.35)',
     delay: 1.5,
-    repeat: 10,
+    repeat: 9,
     repeatDelay: 5
   }, .05);
 };
 
-LogoAnimation.prototype.show = () => {
-  if (!animationProgress) {
 
-    animationProgress = true;
+const LogoAnimation = {
+  init: (logoElement, duration = 1, showText) => {
+    this.elementId                     = logoElement;
+    this.easeEff                       = Back.easeOut.config(2);
+    this.easeEff2                      = Back.easeInOut.config(2);
+    this.getLogoBase                   = $(`#${this.elementId}`);
+    this.getCenterPixel                = $(`#${this.elementId} ._logo_center_pixel`);
+    this.getTagLeft                    = $(`#${this.elementId} ._logo_tag_left`);
+    this.getTagRight                   = $(`#${this.elementId} ._logo_tag_right`);
+    this.getTags                       = $(`#${this.elementId} ._logo_tag_left, #${this.elementId} ._logo_tag_right`);
+    this.getText                       = $(`#${this.elementId} ._logo_text`);
+    this.getSubText                    = $(`#${this.elementId} ._logo_subtext`);
+    this.getPixelback                  = $(`#${this.elementId} ._logo_pixel_back`);
+    this.getPixelbackColor             = $(`#${this.elementId} ._logo_pixel_back ._logo_pixel_back_color`);
+    this[`tl${this.elementId}`]        = new TimelineLite();
+    this[`tl2${this.elementId}`]       = new TimelineLite();
+    this[`tl3${this.elementId}`]       = new TimelineLite();
+    this[`duration_${this.elementId}`] = duration;
+    this[`showText_${this.elementId}`] = showText;
 
-    tl0.to(getLogoBase, 0, {opacity: 1, onComplete: () => {
+    // Pause
+    this[`tl${this.elementId}`].pause();
+
+    if (!this.animationProgress) {
+      this.animationProgress = true;
+
       // Center Animation
-      tl1.to( getCenterPixel, getDuration, {ease: easeEff, scale: 1, rotation: '315'});
+      this[`tl${this.elementId}`].to( this.getCenterPixel, this[`duration_${this.elementId}`], {
+        ease: this.easeEff, scale: 1, rotation: '315'
+      });
 
       // Tags Animation
-      tl2.to( getTags, 1, {ease: easeEff, scale: 1, opacity: 1, x: '0%', delay: getDuration / 2});
+      this[`tl${this.elementId}`].to( this.getTags, this[`duration_${this.elementId}`], {
+        ease: this.easeEff, scale: 1, opacity: 1, x: '0%'
+      }, '-=0.5');
 
       // PixelBack Animation
-      tl3.staggerTo( getPixelback, 1, {ease: easeEff2, scale: 1, opacity: 1, rotation: '135', delay: getDuration / 3}, .05);
+      this[`tl${this.elementId}`].staggerTo( this.getPixelback, this[`duration_${this.elementId}`], {
+        ease: this.easeEff2, scale: 1, opacity: 1, rotation: '135'}, .05, '-=0.8');
 
       // Text Animation
-      if (getShowText === 'true') {
-        tl4.to( getText, 1, {opacity: 1, top: '410%', delay: getDuration / 2});
-        tl5.to( getSubText, 1, {opacity: 1, top: '1330%', delay: getDuration, onComplete: () => {
+      if (this[`showText_${this.elementId}`] === 'true') {
+        this[`tl${this.elementId}`].to( this.getText, this[`duration_${this.elementId}`], {opacity: 1, top: '410%'}, '-=0.5');
+        this[`tl${this.elementId}`].to( this.getSubText, this[`duration_${this.elementId}`], {opacity: 1, top: '1330%', onComplete: () => {
           pixelShine();
-        }});
+        }}, '-=0.5');
       }
 
-    // Ended animation Progress
-    animationProgress = false;
-    }});
-  }
-  if (animationInit) {
-    tl1.restart();
-    tl2.restart();
-    tl3.restart();
-    tl4.restart();
-    tl5.restart();
-  } else {
-    animationInit = true;
-    tl0.play();
-    tl1.play();
-    tl2.play();
-    tl3.play();
-    tl4.play();
-    tl5.play();
+      // Ended animation Progress
+      this.animationProgress = false;
+
+    }
+  },
+  show: () => {
+    resetLogoStyle();
+    TweenLite.to(this.getLogoBase, 0, {opacity: 1});
+    if (this.animationInit) {
+      this[`tl${this.elementId}`].restart();
+    } else {
+      this[`tl${this.elementId}`].play();
+      this.animationInit = true;
+    }
+  },
+  hide: () => {
+    if (this.animationInit) {
+      TweenLite.to(this.getLogoBase, 1, {opacity: 0});
+    }
   }
 };
-
-LogoAnimation.prototype.hide = () => {
-  tl0.to(getLogoBase, .5, {opacity: 0});
-};
-
 
 export { LogoAnimation };
--- src/app/shared/logo-cip/logo-cip.component.ts
+++ src/app/shared/logo-cip/logo-cip.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
-// import * as $ from 'jquery';
+
+import { TweenLite } from 'gsap';
+import * as $ from 'jquery';
 
 import { LogoAnimation } from '../../core/animation/logo.animation';
 
@@ -9,20 +11,16 @@ import { LogoAnimation } from '../../core/animation/logo.animation';
   styleUrls: ['./logo-cip.component.scss']
 })
 export class LogoCipComponent implements OnInit, AfterViewInit {
-  @Input() logoImage:    String = '';
-  @Input() type:         String = '';
-  @Input() size:         String = '';
-  @Input() logoId:       String = '_logo_cip';
-  @Input() animation:    any    = null;
-  @Input() delayShow:    any    = 0;
-  @Input() delayHide:    any    = 0;
+  @Input() logoId:       any    = '_logo_cip';
   @Input() showText:     any    = '';
+  @Input() size:         any    = '';
+  @Input() animation:    any    = '';
+  @Input() duration:     any    = 1;
+  @Input() delay:        any    = 300;
   @Input() setWatermark: any    = '';
 
   pixelsBack = ['lpx0', 'lpx1', 'lpx2', 'lpx3', 'lpx4', 'lpx5', 'lpx6', 'lpx7', 'lpx8', 'lpx9', 'lpx10', 'lpx11', 'lpx12', 'lpx13'];
   logoSize: String;
-  sizeImage: any;
-  startStop: Boolean = false;
   Mylogo;
   watermark: any;
 
@@ -32,41 +30,45 @@ export class LogoCipComponent implements OnInit, AfterViewInit {
     // Watermark
     this.watermark = (this.setWatermark === 'true' ? '_logo_opacity01' : '');
 
-    // Set Logo Image
-    this.sizeImage = (this.logoImage ? {
-      'width': `${this.logoImage}px`
-    } : null);
-
     // Set Logo Size
     this.logoSize = `_logo_cip_${this.size}`;
   }
 
   ngAfterViewInit() {
+    this.execAnimation(this.Mylogo);
+  }
+
+  execAnimation(logoId) {
     // Se não for logo com imagem
-    if (!this.sizeImage) {
-
-      this.Mylogo = new LogoAnimation(this.logoId, 1, this.showText);
-
-      // Mostrar logo
+    if (this.animation) {
       if (this.animation === 'show') {
+        TweenLite.to($(`#${this.logoId}`), 0, {opacity: 0});
+
         const delayShowLogo = setTimeout(() => {
 
-          this.Mylogo.show();
+          this.execLogo('show', this.logoId);
           clearTimeout(delayShowLogo);
 
-        }, this.delayShow);
+        }, this.delay);
       }
-      // Esconder logo
       if (this.animation === 'hide') {
         const delayHideLogo = setTimeout(() => {
 
-          this.Mylogo.hide();
+          this.execLogo('hide', this.logoId);
           clearTimeout(delayHideLogo);
 
-        }, this.delayHide);
+        }, this.delay);
       }
-    } else {
-      // code
+    }
+  }
+
+  execLogo(type, logoId) {
+    LogoAnimation.init(logoId, this.duration, this.showText);
+    if (type === 'show') {
+      LogoAnimation.show();
+    }
+    if (type === 'hide') {
+      LogoAnimation.hide();
     }
   }
 
