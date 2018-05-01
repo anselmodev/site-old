2353191860ab3b9f7903c1d395b1ae9a8eb7e704
CodeInPixel Studios <codeinpixel@gmail.com>
2018-05-01 02:22:08 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: 'AIzaSyAp83OMRDSPDu_652hzu35xHqgcFvjjo2Q',
-    authDomain: 'codeinpixel-site.firebaseapp.com',
-    databaseURL: 'https://codeinpixel-site.firebaseio.com',
-    projectId: 'codeinpixel-site',
-    storageBucket: 'codeinpixel-site.appspot.com',
-    messagingSenderId: '317508851109'
-  }
+  production: true
 };
